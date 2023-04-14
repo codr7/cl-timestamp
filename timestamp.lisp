3893e90bdc2e5e207ee7d621896f66e66398d45a
codr7 <codr7@protonmail.com>
2023-04-14 22:38:08 +0200

    Add leap-year & days-in-month.

--- timestamp.lisp
+++ timestamp.lisp
@@ -1,18 +1,19 @@
 (defpackage timestamp
   (:use cl)
-  (:export day
+  (:export day days-in-month
 	   get-month
 	   hours
-	   microseconds minutes month
+	   leap-year?
+	   microseconds minutes month month-index
 	   new-timestamp 
 	   seconds
-	   timestamp timestamp=
+	   timestamp timestamp= timestamp< timestamp>
 	   year
 	   test))
 
 (in-package timestamp)
 
-(defparameter months '#(:jan :feb :mar :apr :may :jun :jul :aug :sep :oct :nov :dec))
+(defparameter +months+ '#(:n/a :jan :feb :mar :apr :may :jun :jul :aug :sep :oct :nov :dec))
 
 (defstruct (timestamp (:conc-name nil))
   (year (error "Missing year") :type integer)
@@ -33,9 +34,30 @@
 	  (year val) (month val) (day val)
 	  (hours val) (minutes val) (seconds val) (microseconds val)))
 
-(defun get-month (i)
-  (aref months (1- i)))
+(defun leap-year? (year)
+  (and (zerop (mod year 4)) (not (zerop (mod year 100)))))
 
+(defun days-in-month (month year)
+  (ecase month
+    (:jan 31)
+    (:feb (if (leap-year? year) 29 28))
+    (:mar 31)
+    (:apr 30)
+    (:may 31)
+    (:jun 30)
+    (:jul 31)
+    (:aug 31)
+    (:sep 30)
+    (:oct 31)
+    (:nov 30)
+    (:dec 31)))
+  
+(defun get-month (i)
+  (aref +months+ i))
+
+(defun month-index (month)
+  (position month +months+))
+  
 (defun timestamp= (x y)
   (and (= (year x) (year y))
        (eq (month x) (month y))
@@ -45,6 +67,34 @@
        (= (seconds x) (seconds y))
        (= (microseconds x) (microseconds y))))
 
+(defun timestamp< (x y)
+  (or (< (year x) (year y))
+      (< (month-index (month x)) (month-index (month y)))
+      (< (day x) (day y))
+      (< (hours x) (hours y))
+      (< (minutes x) (minutes y))
+      (< (seconds x) (seconds y))
+      (< (microseconds x) (microseconds y))))
+
+(defun timestamp> (x y)
+  (or (> (year x) (year y))
+      (> (month-index (month x)) (month-index (month y)))
+      (> (day x) (day y))
+      (> (hours x) (hours y))
+      (> (minutes x) (minutes y))
+      (> (seconds x) (seconds y))
+      (> (microseconds x) (microseconds y))))
+
 (defun test ()
-  (let ((ts (new-timestamp)))
-    (assert (timestamp= ts ts))))
+  (let ((x (new-timestamp)))
+    (assert (timestamp= x x)))
+
+  (let ((x (new-timestamp 2000 :jan 1))
+	(y (new-timestamp 2000 :jan 2)))
+    (assert (timestamp< x y))
+    (assert (not (timestamp< y x)))
+    (assert (timestamp> y x))
+    (assert (not (timestamp> x y))))
+
+  (assert (= (days-in-month :feb 2000) 28))
+  (assert (= (days-in-month :feb 2016) 29)))
